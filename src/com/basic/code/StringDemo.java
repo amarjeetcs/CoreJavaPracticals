52c45263ce10274cb918464be1b2e30085b6917f
Amarjeet Singh <amarjeetcs79@gmail.com>
2022-12-23 23:07:37 +0530

    This is my string practical

---
+++ src/com/basic/code/StringDemo.java
@@ -0,0 +1,31 @@
+package com.basic.code;
+
+public class StringDemo {
+	
+	
+	public static void main(String[] args) {
+
+    String str=new String("Lalit");
+    String str1=new String("Amarjeet");
+    
+    String str2="Lalit";
+    String str3="Amarjeet";
+    
+    String str4="Lalit";
+    String str5="amarjeet";
+    String str6=new String("Lalit");
+    
+    System.out.println(str==str1);
+    System.out.println(str2==str4);
+    System.out.println(str3==str5);
+    System.out.println(str==str6);
+    System.out.println(str4==str6);
+    System.out.println(str1==str5);
+    
+    System.out.println(str.equals(str6));
+    System.out.println(str1.equals(str5));
+		
+		
+	}
+
+}
